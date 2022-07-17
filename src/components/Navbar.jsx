1d47ecd12b9d87299b88738ac879cd7ace787111
Robert Velasquez <rvela042@fiu.edu>
2022-07-17 03:59:05 -0400

    adding the sidebar and the navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
 import styled from "styled-components";
 import { BiChevronDown } from "react-icons/bi";
 export default function Navbar() {
-  return <Section></Section>;
+  return (
+    <Nav>
+      <h2>Overview</h2>
+      <div className="timeline">
+        <span>Last 30 Days</span>
+        <BiChevronDown />
+      </div>
+    </Nav>
+  );
 }
 
-const Section = styled.section``;
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  color: white;
+  margin: 2rem;
+  .timeline{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    svg {
+      color: var(-- primary-color;);
+      font-size: 2rem;
+    }
+
+  }
+`;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,33 @@ import { AiOutlineDollarCircle } from "react-icons/ai";
 import { SiAnalogue } from "react-icons/si";
 import { FiHelpCircle } from "react-icons/fi";
 export default function Sidebar() {
-  return <Aside></Aside>;
+  return (
+  <Aside id="sidebar">
+    <div className="brand">
+      <RiNeteaseCloudMusicLine />
+    </div>
+    <ul className="links">
+      <li className="selected">
+        <SiAnalogue />
+      </li>
+      <li>
+        <HiOutlineMusicNote />
+      </li>
+      <li>
+        <BiBell />
+      </li>
+      <li>
+        <AiOutlineDollarCircle />
+      </li>
+      <li><BiGroup />
+      </li>
+    </ul>
+    <div className="help">
+      <FiHelpCircle />
+    </div>
+
+    </Aside>
+  );
 }
 
 const Aside = styled.section`
@@ -16,4 +42,59 @@ const Aside = styled.section`
   padding: 2rem;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  .help {
+    svg {
+      cursor: pointer;
+      color: white;
+      transition: 0.5s ease-in-out;
+    }
+    &:hover{
+      svg{
+        color:var(--primary-color);
+      }
+    }
+  }
+  svg {
+    color:var(--primary-color);
+    font-size: 1.5rem
+  }
+  .brand{
+    svg{
+      font-size: 2.5rem;
+    }
+  }
+  .links {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    list-style-type: none;
+    li {
+      border-radius: 1rem;
+      padding: 0.5rem;
+      transition: 0.5s ease-in-out;
+      cursor: pointer;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      svg {
+        color: white;
+      }
+      &:hover {
+        box-shadow:0 0 60px 8px var(--primary-color);
+        svg{
+          color: var(--primary-color);
+        }
+      }
+    }
+    .selected {
+      box-shadow:0 0 60px 8px var(--primary-color);
+        svg{
+          color: var(--primary-color);
+        }
+    }
+  }
+  
+
 `;
