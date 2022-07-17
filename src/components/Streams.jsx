4ed3fc367d5e98768783fc0c658aa826a8841b4d
Robert Velasquez <rvela042@fiu.edu>
2022-07-17 04:51:31 -0400

    created the table of streams with its data

--- src/components/Streams.jsx
+++ src/components/Streams.jsx
@@ -180,7 +180,93 @@ export default function Streams() {
       serviceName: "Apple Music",
     },
   ];
-  return <Section></Section>;
+  return <Section>
+    <div className="title-container">
+      <div className="title">
+        <h4>Streams</h4>
+        <h1>339,567</h1>
+      </div>
+      <div className="slider">
+        <div className="services">
+          {sliderData.map(({image,serviceName}) => {
+            return (
+              <div className="service" key={serviceName}>
+                <img src={image} alt={serviceName} />
+                <h6>{serviceName}</h6>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+    <div className="chart">
+      <ResponsiveContainer height="100%" width="100%">
+        <AreaChart data={data}>
+          <defs>
+            <linearGradient id="colorview" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="10%" 
+              stopColor="var(--primary-color)" 
+              stopOpacity={0.4}
+              ></stop>
+              <stop offset="10%" 
+              stopColor="#000000ff" 
+              stopOpacity={0.2}
+              ></stop>
+            </linearGradient>
+          </defs>
+          <Tooltip />
+          <Area
+          type="monotone"
+          dataKey="data"
+          stroke="var(--primary-color)"
+          strokeWidth={2}
+          fill="url(colorview)"
+          animationBegin={800}
+          animationDuration={2000}
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  </Section>;
 }
 
-const Section = styled.section``;
+const Section = styled.section`
+${applyCardStyles}
+color:white;
+.title-container {
+  display: flex;
+  justify-content: space-between;
+  .title{
+    h1{
+      font-size: 2rem;
+      letter-spacing: 0.2rem;
+    }
+  }
+  .slider {
+    .services {
+      display: flex;
+      gap: 1rem;
+      .service{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 0.6rem;
+        min-width: 5rem;
+        img{
+          height: 2rem;
+        }
+      }
+    }
+  }
+}
+.chart{
+  height: 10rem;
+  .recharts-default-tooltip {
+    background-color: var(--dark-background-color) !important;
+    border: none !important;
+    border-radius: 1rem;
+    box-shadow: 0 0 60px 8px var(--primary-color);
+  }
+}
+`;
