2fa561c0ec86f81871bfb11bac92990ec275c9ac
Robert Velasquez <rvela042@fiu.edu>
2022-07-23 01:32:14 -0400

    added the target audience card

--- src/components/Streams.jsx
+++ src/components/Streams.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 import { applyCardStyles } from "components/ReusableStyles";
 import { AreaChart, Area, Tooltip, ResponsiveContainer } from "recharts";
@@ -6,16 +7,17 @@ import logo1 from "assets/logo1.png";
 import logo2 from "assets/logo2.png";
 import logo3 from "assets/logo3.png";
 import logo4 from "assets/logo4.png";
-export default function Streams() {
+
+function Streams() {
   const data = [
     {
-      data: 3780,
+      data: 6780,
     },
     {
-      data: 4680,
+      data: 6680,
     },
     {
-      data: 5500,
+      data: 6500,
     },
     {
       data: 6300,
@@ -24,7 +26,7 @@ export default function Streams() {
       data: 5900,
     },
     {
-      data: 7000,
+      data: 5700,
     },
     {
       data: 5500,
@@ -41,9 +43,7 @@ export default function Streams() {
     {
       data: 5300,
     },
-    { 
-      data: 4500 
-    },
+    { data: 4500 },
     {
       data: 5000,
     },
@@ -51,7 +51,7 @@ export default function Streams() {
       data: 4700,
     },
     {
-      data: 2400,
+      data: 4400,
     },
     {
       data: 4800,
@@ -108,7 +108,7 @@ export default function Streams() {
       data: 5800,
     },
     {
-      data: 1000,
+      data: 6000,
     },
     {
       data: 6300,
@@ -182,93 +182,118 @@ export default function Streams() {
       serviceName: "Apple Music",
     },
   ];
-  return <Section>
-    <div className="title-container">
-      <div className="title">
-        <h4>Streams</h4>
-        <h1>339,567</h1>
-      </div>
-      <div className="slider">
-        <div className="services">
-          {sliderData.map(({image,serviceName}) => {
-            return (
-              <div className="service" key={serviceName}>
-                <img src={image} alt={serviceName} />
-                <h6>{serviceName}</h6>
-              </div>
-            );
-          })}
+  return (
+    <Section>
+      <div className="title-container">
+        <div className="title">
+          <h4>Streams</h4>
+          <h1>450,210</h1>
+        </div>
+        <div className="slider">
+          <div className="services">
+            {sliderData.map(({ image, serviceName }) => {
+              return (
+                <div className="service" key={serviceName}>
+                  <img src={image} alt={serviceName} />
+                  <h6>{serviceName}</h6>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-    <div className="chart">
-      <ResponsiveContainer height="100%" width="100%">
-        <AreaChart data={data}>
-          <defs>
-            <linearGradient id="colorview" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="10%" 
-              stopColor="var(--primary-color)" 
-              stopOpacity={0.4}
-              ></stop>
-              <stop offset="10%" 
-              stopColor="#000000ff" 
-              stopOpacity={0.2}
-              ></stop>
-            </linearGradient>
-          </defs>
-          <Tooltip />
-          <Area
-          type="monotone"
-          dataKey="data"
-          stroke="var(--primary-color)"
-          strokeWidth={2}
-          fill="url(colorview)"
-          animationBegin={800}
-          animationDuration={2000}
-          />
-        </AreaChart>
-      </ResponsiveContainer>
-    </div>
-  </Section>;
+      <div className="chart">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            //  width={500} height={400}
+            data={data}
+          >
+            <defs>
+              <linearGradient id="colorview" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="10%"
+                  stopColor="var(--primary-color)"
+                  stopOpacity={0.4}
+                />
+                <stop offset="100%" stopColor="#000000ff" stopOpacity={0.2} />
+              </linearGradient>
+            </defs>
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="data"
+              stroke="var(--primary-color)"
+              strokeWidth={2}
+              fill="url(#colorview)"
+              animationBegin={800}
+              animationDuration={2000}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    </Section>
+  );
 }
 
 const Section = styled.section`
-${applyCardStyles}
-color:white;
-.title-container {
-  display: flex;
-  justify-content: space-between;
-  .title{
-    h1{
-      font-size: 2rem;
-      letter-spacing: 0.2rem;
+  ${applyCardStyles}
+  color:white;
+  .title-container {
+    display: flex;
+    justify-content: space-between;
+    .title {
+      h1 {
+        font-size: 2rem;
+        letter-spacing: 0.2rem;
+      }
     }
-  }
-  .slider {
-    .services {
-      display: flex;
-      gap: 1rem;
-      .service{
+    .slider {
+      .services {
         display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        gap: 0.6rem;
-        min-width: 5rem;
-        img{
-          height: 2rem;
+        gap: 1rem;
+        .service {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          gap: 0.6rem;
+          min-width: 5rem;
+          img {
+            height: 2rem;
+          }
         }
       }
     }
   }
-}
-.chart{
-  height: 10rem;
-  .recharts-default-tooltip {
-    background-color: var(--dark-background-color) !important;
-    border: none !important;
-    border-radius: 1rem;
-    box-shadow: 0 0 60px 8px var(--primary-color);
+  .chart {
+    height: 10rem;
+    .recharts-default-tooltip {
+      background-color: var(--dark-background-color) !important;
+      border: none !important;
+      border-radius: 1rem;
+      box-shadow: 0 0 60px 8px var(--primary-color);
+    }
   }
-}
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    height: 100%;
+    .title-container {
+      flex-direction: column;
+      gap: 0.5rem;
+      .title {
+        text-align: center;
+      }
+      .slider {
+        .services {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          .service {
+            gap: 0.5rem;
+            min-width: 1rem;
+          }
+        }
+      }
+    }
+  }
 `;
+
+export default Streams;
